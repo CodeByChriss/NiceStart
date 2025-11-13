38188d836fa826c43da36ff4f542624c8030f0da
ChristianSalguero <salguerovaras777@gmail.com>
2025-11-13 13:55:08 +0100

    appCompat removed from login and singUp activities

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -22,11 +22,6 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         ImageView mGirl = findViewById(R.id.ivBackViewGirl);
 
--- app/src/main/java/com/example/nicestart/Signup.java
+++ app/src/main/java/com/example/nicestart/Signup.java
@@ -23,11 +23,6 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         ImageView mBoy = findViewById(R.id.ivBackViewBoy);
 
